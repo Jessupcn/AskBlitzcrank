9c0d0d3324cb86faa5b5c137109de779214aeb72
Connor Jessup <cjessup@umich.edu>
2021-02-20 12:02:33 -0500

    clash search

--- utils/makePercent.js
+++ utils/makePercent.js
@@ -3,9 +3,9 @@ const makePercent = (numerator, denominator) => {
   let numSplit = String(value).split('.');
   console.log('NUM SPLIT', numSplit.length, numSplit[1].length)
 
-  console.log('test:', numSplit.length === 1, numSplit[1].length > 2)
+  console.log('test:', numSplit.length === 1, numSplit[1]?.length > 2)
   // If there is no decimal point or only one decimal place found.
-    if (numSplit.length === 1 || numSplit[1].length > 2) {
+    if (numSplit.length === 1 || numSplit[1]?.length > 2) {
         // Set the number to two decimal places
         value = value.toFixed(2);
     }
