b9af0a0afb48088e17f97871c1be2cff963797ad
Connor Jessup <cjessup@umich.edu>
2021-02-20 12:13:41 -0500

    makePercent util

--- utils/makePercent.js
+++ utils/makePercent.js
@@ -1,16 +1,5 @@
 const makePercent = (numerator, denominator) => {
-  let value = Number((numerator / denominator) * 100);
-  let numSplit = String(value).split('.');
-  console.log('NUM SPLIT', numSplit.length, numSplit[1] ? numSplit[1].length  : '')
-
-  console.log('test:', numSplit.length === 1, numSplit[1] ? numSplit[1].length > 2 : '')
-  // If there is no decimal point or only one decimal place found.
-    if (numSplit.length > 1 || numSplit[1]?.length > 2) {
-        // Set the number to two decimal places
-        value = value.toFixed(2);
-    }
-  // Return updated or original number.
-  return value;
+  return Number((numerator / denominator) * 100)
 }
 
 module.exports = makePercent;
