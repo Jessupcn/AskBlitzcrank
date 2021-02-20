4e63d984aae85c76c1fe5c5b4fce373f4faa8295
Connor Jessup <cjessup@umich.edu>
2021-02-20 12:09:01 -0500

    clash search

--- utils/makePercent.js
+++ utils/makePercent.js
@@ -1,7 +1,7 @@
 const makePercent = (numerator, denominator) => {
   let value = Number((numerator / denominator) * 100);
   let numSplit = String(value).split('.');
-  console.log('NUM SPLIT', numSplit.length, numSplit[1].length)
+  console.log('NUM SPLIT', numSplit.length, numSplit[1] ? numSplit[1].length  : '')
 
   console.log('test:', numSplit.length === 1, numSplit[1] ? numSplit[1].length > 2 : '')
   // If there is no decimal point or only one decimal place found.
