35a4906b4c1c022f332ef6394dc91e01bb1abadc
Connor Jessup <cjessup@umich.edu>
2021-02-20 12:34:15 -0500

    makePercent util

--- utils/makePercent.js
+++ utils/makePercent.js
@@ -1,5 +1,5 @@
 const makePercent = (numerator, denominator) => {
-  return Number((numerator / denominator) * 100)
+  return Number((numerator / denominator) * 100).toFixed(2);
 }
 
 module.exports = makePercent;
